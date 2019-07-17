567ffaaf072a0515fa92199fe1de52ceb1f7d2f4
Ryan <ryan4work@163.com>
2019-07-17 23:42:30 +0800

    commit 1. 修改TitleText 2. 修改公众号二维码，赞赏码

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -1,12 +1,12 @@
 /**
  * 打赏二维码
  */
-var moneyUrl ="https://test-91f3af.tcb.qcloud.la/common/WechatIMG66.jpeg?sign=38e2cccbf86dd602ae575c89b2911b16&t=1556369699"
+var moneyUrl ="https://7465-test-3rcvg-1259137164.tcb.qcloud.la/QcCode/mony.jpeg?sign=2e87100ffc10834da75ee0565cb1319d&t=1563376235"
 
 /**
  * 公众号二维码
  */
-var wechatUrl ="https://test-91f3af.tcb.qcloud.la/common/WechatIMG2.jpeg?sign=e81a38eec6cebfc82c1c34bb7e233bae&t=1556369822"
+var wechatUrl ="https://7465-test-3rcvg-1259137164.tcb.qcloud.la/QcCode/qrcode_for_wuzhuqingshi.jpg?sign=6f7c1622dffe6c53fee5f0080eb0548a&t=1563378006"
 
 /**
  * 云开发环境
